5e2d7dbcb75ef08ee51868961bd92db2626ffed0
Amos Wenger <amoswenger@gmail.com>
2016-05-17 14:32:30 +0200

    Don't die in infinite recursion land

--- libfake.c
+++ libfake.c
@@ -1,6 +1,7 @@
 
 #define _GNU_SOURCE
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <dlfcn.h>
 #include <stdint.h>
@@ -19,6 +20,11 @@ int SDL_Init (uint32_t flags) {
       void *our_sdl_init = dlsym(RTLD_DEFAULT, "SDL_Init");
       printf("[libfake] dls sdl init = %p\n", our_sdl_init);
       printf("[libfake] ptr sdl init = %p\n", &SDL_Init);
+
+      if (Original_SDL_Init == &SDL_Init) {
+        printf("[libfake] can't find original SDL init! bailing out\n");
+        exit(1);
+      }
     }
 
     printf("[libfake] Calling original SDL_Init with %x\n", flags);
--- main.c
+++ main.c
@@ -4,11 +4,20 @@
 
 int main(int argc, char *argv[]) {
   printf("[main] Calling SDL_Init\n");
-  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Init(SDL_INIT_VIDEO);
   printf("[main] Returned from SDL_Init\n");
 
+  printf("[main] Asking for OpenGL 3.2 context\n");
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+  printf("[main] Sleeping for a second\n");
   SDL_Delay(1000);
 
+  printf("[main] Quitting\n");
   SDL_Quit();
+
   return 0;
 }
