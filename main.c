be627096737e1d2bca1fed5104845dae0e30ed1b
Amos Wenger <amoswenger@gmail.com>
2016-05-17 15:02:23 +0200

    Simple vertex + fragment shader

--- main.c
+++ main.c
@@ -5,12 +5,25 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 
-void assert (const char *msg, void *cond) {
+#define SHADER_LEN 4096
+
+void assert (const char *msg, int cond) {
   if (cond) {
     return;
   }
   fprintf(stderr, "[main] Assertion failed: %s\n", msg);
   exit(1);
+}
+
+void readFile (char *target, const char *path) {
+  FILE *f = fopen(path, "r");
+  assert("Opened shader file successfully", !!f);
+
+  size_t read = fread(target, 1, SHADER_LEN, f);
+  assert("Read shader successfully", read > 0);
+
+  fclose(f);
+  return;
 }
 
 int main(int argc, char *argv[]) {
@@ -25,19 +38,68 @@ int main(int argc, char *argv[]) {
   SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
   SDL_Window* window = SDL_CreateWindow("opengl-inject-poc", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
-  assert("Window created correctly", window);
+  assert("Window created correctly", !!window);
 
   SDL_GLContext context = SDL_GL_CreateContext(window);
-  assert("OpenGL context created correctly", context);
+  assert("OpenGL context created correctly", !!context);
 
   printf("[main] Initializing glew...\n");
   glewExperimental = GL_TRUE;
   glewInit();
 
+  float vertices[] = {
+    0.0f,  0.5f, // Vertex 1 (X, Y)
+    0.5f, -0.5f, // Vertex 2 (X, Y)
+    -0.5f, -0.5f  // Vertex 3 (X, Y)
+  };
+
   printf("[main] Making a vertex buffer...\n");
-  GLuint vertexBuffer;
-  glGenBuffers(1, &vertexBuffer);
-  printf("[main] Vertex buffer: %u\n", vertexBuffer);
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  printf("[main] Vertex buffer: %u\n", vbo);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  char* vertexSource = malloc(SHADER_LEN);
+  bzero(vertexSource, SHADER_LEN);
+  readFile(vertexSource, "shader.vert");
+  printf("vertex source: %s\n", vertexSource);
+
+  char* fragmentSource = malloc(SHADER_LEN);
+  bzero(fragmentSource, SHADER_LEN);
+  readFile(fragmentSource, "shader.frag");
+  printf("fragment source: %s\n", fragmentSource);
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, (void*) &vertexSource, NULL);
+  glCompileShader(vertexShader);
+
+  GLint status;
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+
+  if (status != GL_TRUE) {
+    char buffer[512];
+    glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
+    fprintf(stderr, "Vertex shader compile log: %s\n", buffer);
+
+    assert("Vertex shader compiled", status == GL_TRUE);
+  }
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, (void*) &fragmentSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+
+  if (status != GL_TRUE) {
+    char buffer[512];
+    glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
+    fprintf(stderr, "Fragment shader compile log: %s\n", buffer);
+
+    assert("Fragment shader compiled", status == GL_TRUE);
+  }
 
   printf("[main] Sleeping for a second\n");
   SDL_Delay(1000);
---
+++ shader.vert
@@ -0,0 +1,9 @@
+#version 150
+
+in vec2 position;
+
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+
