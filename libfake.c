e3e2cfcfef8d869d52b15718831a485830ef8f4c
Amos Wenger <amoswenger@gmail.com>
2016-05-17 13:46:59 +0200

    load/unload callbacks

--- libfake.c
+++ libfake.c
@@ -1,4 +1,17 @@
 
+#include <stdio.h>
+
+static void con() __attribute__((constructor));
+static void des() __attribute__((destructor));
+
+void con() {
+    printf("I'm a constructor\n");
+}
+
+void des() {
+    printf("I'm a destructor\n");
+}
+
 double cos (double x) {
   return 0.69;
 }
