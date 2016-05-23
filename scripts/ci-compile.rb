4060351e03c79a03b7d2e9da9ec762366ecaea94
Amos Wenger <amoswenger@gmail.com>
2016-05-23 17:29:27 +0200

    Add CI scripts for osx

--- scripts/ci-compile.rb
+++ scripts/ci-compile.rb
@@ -4,11 +4,40 @@
 require_relative 'common'
 
 module Capsule
-  def Capsule.ci_compile
+  def Capsule.ci_compile (os)
     say "Compiling #{app_name}"
 
-    ✓ sh 'cmake'
-    ✓ sh 'msbuild'
+    case os
+    when "windows"
+      ci_compile_windows
+    when "darwin"
+      ci_compile_darwin
+    end
+  end
+
+  def Capsule.ci_compile_windows
+    FileUtils.rm_rf 'build32'
+    FileUtils.mkdir_p 'build32'
+    cd 'build32' do
+      ✓ sh 'cmake -G "Visual Studio 14 2015" ..'
+      ✓ sh 'msbuild ALL_BUILD.vcxproj'
+    end
+
+    FileUtils.rm_rf 'build64'
+    FileUtils.mkdir_p 'build64'
+    cd 'build64' do
+      ✓ sh 'cmake -G "Visual Studio 14 2015 Win64" ..'
+      ✓ sh 'msbuild ALL_BUILD.vcxproj'
+    end
+  end
+
+  def Capsule.ci_compile_darwin
+    FileUtils.rm_rf 'build'
+    FileUtils.mkdir_p 'build'
+    cd 'build' do
+      ✓ sh 'cmake -DOSX_UNIVERSAL=1 ..'
+      ✓ sh 'make'
+    end
   end
 end
 
