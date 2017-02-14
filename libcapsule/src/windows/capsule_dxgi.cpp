481f673954968926249c1946e943457b431987f2
Amos Wenger <amos@itch.io>
2017-02-14 20:37:36 +0100

    Hook D3D11CreateDeviceAndSwapChain (which will probably ultimately prove futile)

--- libcapsule/src/windows/capsule_dxgi.cpp
+++ libcapsule/src/windows/capsule_dxgi.cpp
@@ -18,6 +18,7 @@ HRESULT CAPSULE_STDCALL CreateDXGIFactory_hook (REFIID riid, void** ppFactory) {
 // D3D10CreateDeviceAndSwapChain
 ///////////////////////////////////////////////
 
+#include <D3D10_1.h>
 #include <D3D10Misc.h>
 
 typedef HRESULT (CAPSULE_STDCALL *D3D10CreateDeviceAndSwapChain_t)(
@@ -45,6 +46,50 @@ HRESULT D3D10CreateDeviceAndSwapChain_hook (
 ) {
   capsule_log("Intercepted D3D10CreateDeviceAndSwapChain!");
   return D3D10CreateDeviceAndSwapChain_real(pAdapter, DriverType, Software, Flags, SDKVersion, pSwapChainDesc, ppSwapChain, ppDevice);
+}
+
+///////////////////////////////////////////////
+// D3D11CreateDeviceAndSwapChain
+///////////////////////////////////////////////
+
+#include <D3D11.h>
+
+typedef HRESULT (CAPSULE_STDCALL *D3D11CreateDeviceAndSwapChain_t)(
+         IDXGIAdapter         *pAdapter,
+         D3D_DRIVER_TYPE      DriverType,
+         HMODULE              Software,
+         UINT                 Flags,
+   const D3D_FEATURE_LEVEL    *pFeatureLevels,
+         UINT                 FeatureLevels,
+         UINT                 SDKVersion,
+   const DXGI_SWAP_CHAIN_DESC *pSwapChainDesc,
+         IDXGISwapChain       **ppSwapChain,
+         ID3D11Device         **ppDevice,
+         D3D_FEATURE_LEVEL    *pFeatureLevel,
+         ID3D11DeviceContext  **ppImmediateContext
+);
+D3D11CreateDeviceAndSwapChain_t D3D11CreateDeviceAndSwapChain_real;
+SIZE_T D3D11CreateDeviceAndSwapChain_hookId;
+
+HRESULT D3D11CreateDeviceAndSwapChain_hook (
+         IDXGIAdapter         *pAdapter,
+         D3D_DRIVER_TYPE      DriverType,
+         HMODULE              Software,
+         UINT                 Flags,
+   const D3D_FEATURE_LEVEL    *pFeatureLevels,
+         UINT                 FeatureLevels,
+         UINT                 SDKVersion,
+   const DXGI_SWAP_CHAIN_DESC *pSwapChainDesc,
+         IDXGISwapChain       **ppSwapChain,
+         ID3D11Device         **ppDevice,
+         D3D_FEATURE_LEVEL    *pFeatureLevel,
+         ID3D11DeviceContext  **ppImmediateContext
+) {
+  capsule_log("Intercepted D3D11CreateDeviceAndSwapChain!");
+  return D3D11CreateDeviceAndSwapChain_real(pAdapter, DriverType, Software,
+    Flags, pFeatureLevels, FeatureLevels, SDKVersion,
+    pSwapChainDesc, ppSwapChain, ppDevice, pFeatureLevel, ppImmediateContext
+  );
 }
 
 void capsule_install_dxgi_hooks () {
@@ -103,4 +148,31 @@ void capsule_install_dxgi_hooks () {
     return;
   }
   capsule_log("Installed D3D10CreateDeviceAndSwapChain hook");
+
+  // d3d11
+
+  HINSTANCE d3d11 = LoadLibrary(L"d3d11.dll");
+  if (!d3d11) {
+    capsule_log("Could not load d3d11.dll, disabling D3D10/11 capture");
+    return;
+  }
+
+  LPVOID D3D11CreateDeviceAndSwapChain_addr = NktHookLibHelpers::GetProcedureAddress(d3d11, "D3D11CreateDeviceAndSwapChain");
+  if (!D3D11CreateDeviceAndSwapChain_addr) {
+    capsule_log("Could not find D3D11CreateDeviceAndSwapChain");
+    return;
+  }
+
+  err = cHookMgr.Hook(
+    &D3D11CreateDeviceAndSwapChain_hookId,
+    (LPVOID *) &D3D11CreateDeviceAndSwapChain_real,
+    D3D11CreateDeviceAndSwapChain_addr,
+    D3D11CreateDeviceAndSwapChain_hook,
+    0
+  );
+  if (err != ERROR_SUCCESS) {
+    capsule_log("Hooking D3D11CreateDeviceAndSwapChain derped with error %d (%x)", err, err);
+    return;
+  }
+  capsule_log("Installed D3D11CreateDeviceAndSwapChain hook");
 }
