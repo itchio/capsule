3e7e3c08327c265b09928fc88d9e9ae8d0ae6963
Amos Wenger <amoswenger@gmail.com>
2017-02-27 12:47:18 +0100

    Remove old keyCode code

--- libcapsule/src/macos/capsule_NSApplication.mm
+++ libcapsule/src/macos/capsule_NSApplication.mm
@@ -21,26 +21,7 @@
 // static CapsuleFixedRecorder *recorder;
 
 - (void)capsule_sendEvent:(NSEvent*)event {
-//  if (([event type] == NSKeyDown && [event keyCode] == kVK_F9) || ([event type] == NSRightMouseDown)) {
-//    NSLog(@"F9 pressed! %@", event);
-//    if (capsule_capturing) {
-//      [[NSSound soundNamed:@"Pop"] play];
-//      capsule_capturing = 0;
-//      recorder = nil;
-//    } else {
-//      [[NSSound soundNamed:@"Ping"] play];
-//      if (capsule_had_opengl) {
-//        NSLog(@"Starting OpenGL capture");
-//        capsule_capturing = 1;
-//      } else {
-//        NSLog(@"Starting CGWindow capture");
-//        capsule_capturing = 2;
-//        recorder = [[CapsuleFixedRecorder alloc] init];
-//      }
-//    }
-//  } else {
-//    // NSLog(@"Another event: %@", event);
-//  }
+  NSLog(@"NSApplication event: %@", event);
   [self capsule_sendEvent:event];
 }
 
