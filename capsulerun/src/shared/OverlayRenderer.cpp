2cced33f467dc60e9ca66f6977893c9724046b98
Amos Wenger <amos@itch.io>
2017-03-09 16:05:51 +0100

    Actually draw something

--- capsulerun/src/shared/OverlayRenderer.cpp
+++ capsulerun/src/shared/OverlayRenderer.cpp
@@ -22,4 +22,23 @@ OverlayRenderer::OverlayRenderer() {
   if (status != CAIRO_STATUS_SUCCESS) {
     capsule_log("Could not create cairo ceontext: error %d", status);
   }
+
+  cairo_set_source_rgb(cr, 0, 0, 0);
+  cairo_paint(cr);
+
+  cairo_set_line_width(cr, 15);
+  cairo_set_source_rgb(cr, 255, 0, 0);
+  cairo_move_to(cr, 0, -100);
+  cairo_line_to(cr, 100, 100);
+  cairo_rel_line_to(cr, -200, 0);
+  cairo_close_path(cr);
+  cairo_stroke(cr);
+
+  cairo_surface_flush(surface);
+  unsigned char *data = cairo_image_surface_get_data(surface);
+
+  FILE *f = fopen("cairo.rawimg", "wb");
+  fwrite(data, 256 * 4 * 128, 1, f);
+  capsule_log("Wrote raw surface to disk!");
+  fclose(f);
 }
