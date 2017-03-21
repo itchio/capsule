609ddbfb0e2e69b6b9ddaef4683e4e7dfe4eeef8
Amos Wenger <amoswenger@gmail.com>
2017-03-21 23:33:13 +0100

    forgo 'using' in macos hotkey

--- capsulerun/src/macos/hotkey.mm
+++ capsulerun/src/macos/hotkey.mm
@@ -6,18 +6,14 @@
 
 #include "../shared/MainLoop.h"
 
-#include <thread>
-
-using namespace std;
-
 void CapsuleRunApp () {
   [[NSApplication sharedApplication] run];
 }
 
 int CapsuleHotkeyInit(MainLoop *ml) {
-  CapsuleLog("capsule_hotkey_init: registering hotkey");
+  CapsuleLog("CapsuleHotkeyInit: registering hotkey");
 
-  CapsuleLog("capsule_hotkey_init: kVK_F9 is %d", kVK_F9);
+  CapsuleLog("CapsuleHotkeyInit: kVK_F9 is %d", kVK_F9);
   DDHotKey *hotKey = [[DDHotKeyCenter sharedHotKeyCenter] registerHotKeyWithKeyCode:kVK_F9 modifierFlags:0 task:^(NSEvent *ev) {
     ml->CaptureFlip();
   }];
