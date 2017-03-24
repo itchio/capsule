3e594dfc77ba1ca78e9a54e9daa41d813f787f59
Amos Wenger <amos@itch.io>
2017-03-24 20:04:58 +0100

    Copyright disclaimers everywhere! closes #39

--- capsulerun/src/macros.h
+++ capsulerun/src/macros.h
@@ -1,21 +1,26 @@
+
+/*
+ *  capsule - the game recording and overlay toolkit
+ *  Copyright (C) 2017, Amos Wenger
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details:
+ * https://github.com/itchio/capsule/blob/master/LICENSE
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
 #pragma once
 
 #include <stdio.h>
-
-// Change these to suit your needs
-// #define CAPSULERUN_PROFILE
-// #define CAPSULERUN_DEBUG
-
-#ifdef CAPSULERUN_PROFILE
-#define eprintf(...) { fprintf(stdout, "[capsulerun-profile] "); fprintf(stdout, __VA_ARGS__); fprintf(stdout, "\n"); fflush(stdout); }
-#else
-#define eprintf(...)
-#endif
-
-#ifdef CAPSULERUN_DEBUG
-#define cdprintf(...) { fprintf(stdout, "[capsulerun-debug] "); fprintf(stdout, __VA_ARGS__); fprintf(stdout, "\n"); fflush(stdout); }
-#else
-#define cdprintf(...)
-#endif
 
 #define CapsuleLog(...) { fprintf(stdout, "[capsulerun] "); fprintf(stdout, __VA_ARGS__); fprintf(stdout, "\n"); fflush(stdout); }
