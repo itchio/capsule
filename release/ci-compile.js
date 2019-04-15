70de88e0c090eb334b8bc967b7fd5908c059c3bf
Amos Wenger <amoswenger@gmail.com>
2019-04-15 13:18:59 +0200

    ci: build everything, use capsulerun for test

--- release/ci-compile.js
+++ release/ci-compile.js
@@ -5,82 +5,123 @@
 const $ = require("./common");
 const path = require("path");
 
-let libPath = path.join(process.cwd(), "libcapsule");
-let libCargoPath = path.join(libPath, "Cargo.toml");
+let workspacePath = path.join(process.cwd());
+let workspaceCargoPath = path.join(workspacePath, "Cargo.toml");
 
-async function ci_compile (args) {
+async function ci_compile(args) {
   if (args.length < 1) {
-    throw new Error(`ci-compile expects at least one argument, not ${args.length}. (got: ${args.join(", ")})`)
+    throw new Error(
+      `ci-compile expects at least one argument, not ${
+        args.length
+      }. (got: ${args.join(", ")})`
+    );
   }
-  const [os] = args
+  const [os] = args;
 
   let arch = "";
   if (os === "linux") {
     if (args.length !== 2) {
-      throw new Error(`ci-compile expects two arguments (os arch), not ${args.length}. (got: ${args.join(", ")})`)
+      throw new Error(
+        `ci-compile expects two arguments (os arch), not ${
+          args.length
+        }. (got: ${args.join(", ")})`
+      );
     }
     arch = args[1];
   }
 
-  $.say(`Compiling ${$.app_name()}`)
+  $.say(`Compiling ${$.app_name()}`);
   switch (os) {
     case "windows": {
-      await ci_compile_windows()
-      break
+      await ci_compile_windows();
+      break;
     }
     case "darwin": {
-      await ci_compile_darwin()
-      break
+      await ci_compile_darwin();
+      break;
     }
     case "linux": {
-      await ci_compile_linux(arch)
-      break
+      await ci_compile_linux(arch);
+      break;
     }
     default: {
-      throw new Error(`Unsupported os ${os}`)
+      throw new Error(`Unsupported os ${os}`);
     }
   }
 }
 
 function rustArch(arch) {
-    switch (arch) {
-      case "386":   return "i686"
-      case "amd64": return "x86_64"
-    }
-    throw new Error(`Unsupported arch: ${arch}`);
+  switch (arch) {
+    case "386":
+      return "i686";
+    case "amd64":
+      return "x86_64";
+  }
+  throw new Error(`Unsupported arch: ${arch}`);
 }
 
-async function install_rust({name, platform}) {
-  if (!name) { throw new Error("missing name") }
-  if (!platform) { throw new Error("missing platform") }
+async function install_rust({ name, platform }) {
+  if (!name) {
+    throw new Error("missing name");
+  }
+  if (!platform) {
+    throw new Error("missing platform");
+  }
 
   process.env.CARGO_HOME = path.join(process.cwd(), ".cargo");
   process.env.RUSTUP_HOME = path.join(process.cwd(), ".multirust");
   process.env.PATH = `${process.cwd()}/.cargo/bin:${process.env.PATH}`;
-  $(await $.sh(`curl --fail --output "${name}" "https://static.rust-lang.org/rustup/dist/${platform}/${name}"`));
+  $(
+    await $.sh(
+      `curl --fail --output "${name}" "https://static.rust-lang.org/rustup/dist/${platform}/${name}"`
+    )
+  );
   $(await $.sh(`chmod +x "${name}"`));
   $(await $.sh(`"./${name}" --no-modify-path -y`));
 }
 
-async function build_libcapsule({libName, osarch, platform, strip, test}) {
-  if (!libName) { throw new Error("missing libName") }
-  if (!platform) { throw new Error("missing platform") }
-  if (!osarch) { throw new Error("missing osarch") }
+async function build_libcapsule({
+  libName,
+  runName,
+  osarch,
+  platform,
+  strip,
+  test
+}) {
+  if (!libName) {
+    throw new Error("missing libName");
+  }
+  if (!runName) {
+    throw new Error("missing runName");
+  }
+  if (!platform) {
+    throw new Error("missing platform");
+  }
+  if (!osarch) {
+    throw new Error("missing osarch");
+  }
 
   $(await $.sh(`rustup toolchain install "stable-${platform}"`));
-  $(await $.sh(`rustup run "stable-${platform}" cargo build --target "${platform}" --release --manifest-path "${libCargoPath}"`));
+  $(
+    await $.sh(
+      `rustup run "stable-${platform}" cargo build --target "${platform}" --release --manifest-path "${workspaceCargoPath}"`
+    )
+  );
 
   let dest = path.join("compile-artifacts", osarch);
   $(await $.sh(`mkdir -p "compile-artifacts/${osarch}"`));
-  let libFile = `${libPath}/target/${platform}/release/${libName}`;
+  let libFile = `${workspacePath}/target/${platform}/release/${libName}`;
+  let runFile = `${workspacePath}/target/${platform}/release/${runName}`;
   if (strip) {
     $(await $.sh(`strip "${libFile}"`));
+    $(await $.sh(`strip "${runFile}"`));
   }
-  $(await $.sh(`cp -rf "${libFile}" "compile-artifacts/${osarch}"`));
+  $(await $.sh(`cp -rf "${libFile}" "${dest}"`));
+  $(await $.sh(`cp -rf "${runFile}" "${dest}"`));
 
   if (test) {
     await $.cd("test", async () => {
-      $(await $.sh(`gcc puts.c -o puts`))
+      $(await $.sh(`gcc puts.c -o puts`));
       let e;
       try {
         $(await $.sh(`./puts`));
@@ -92,30 +133,33 @@ async function build_libcapsule({libName, osarch, platform, strip, test}) {
       } else {
         throw new Error(`Expected crash, got none`);
       }
-      $(await $.sh(`CAPSULE_TEST=1 ${test}="${libFile}" ./puts > out.txt`))
+      $(await $.sh(`CAPSULE_TEST=1 ${runFile} ./puts > out.txt`));
       let expectedOutput = "caught dead beef";
       let actualOutput = (await $.readFile("out.txt")).trim();
       if (actualOutput == expectedOutput) {
         $.say(`Injection test passed!`);
       } else {
-        throw new Error(`Injection test failed:\nexpected\n${expectedOutput}\ngot:${actualOutput}`);
+        throw new Error(
+          `Injection test failed:\nexpected\n${expectedOutput}\ngot:${actualOutput}`
+        );
       }
-    })
+    });
   }
 }
 
-async function ci_compile_windows () {
+async function ci_compile_windows() {
   await install_rust({
     name: `rustup-init.exe`,
-    platform: `x86_64-pc-windows-msvc`,
+    platform: `x86_64-pc-windows-msvc`
   });
 
   for (const arch of ["386", "amd64"]) {
     let platform = `${rustArch(arch)}-pc-windows-msvc`;
     await build_libcapsule({
       libName: `capsule.dll`,
+      runName: `capsulerun.exe`,
       osarch: `windows-${arch}`,
-      platform,
+      platform
     });
   }
 }
@@ -124,13 +168,13 @@ async function ci_compile_darwin() {
   let platform = `x86_64-apple-darwin`;
   await install_rust({
     name: `rustup-init`,
-    platform,
+    platform
   });
   await build_libcapsule({
     libName: `libcapsule.dylib`,
+    runName: `capsulerun`,
     osarch: `darwin-amd64`,
-    platform,
-    test: "DYLD_INSERT_LIBRARIES",
+    platform
   });
 }
 
@@ -138,19 +182,18 @@ async function ci_compile_linux(arch) {
   let platform = `${rustArch(arch)}-unknown-linux-gnu`;
   await install_rust({
     name: `rustup-init`,
-    platform,
+    platform
   });
   await build_libcapsule({
     libName: `libcapsule.so`,
+    runName: `capsulerun`,
     osarch: `linux-${arch}`,
     platform,
-    strip: true,
-    test: "LD_PRELOAD",
+    strip: true
   });
 }
 
-ci_compile(process.argv.slice(2)).catch((e) => {
+ci_compile(process.argv.slice(2)).catch(e => {
   console.error(e);
   process.exit(1);
 });
-
