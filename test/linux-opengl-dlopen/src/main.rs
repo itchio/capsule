fdc535f2c7a26ab12c20eae8e2a3ac5620b70843
Amos Wenger <amoswenger@gmail.com>
2019-04-18 13:05:12 +0200

    Remove all dependencies from linux-opengl-dlopen

--- test/linux-opengl-dlopen/src/main.rs
+++ test/linux-opengl-dlopen/src/main.rs
@@ -1,21 +1,24 @@
 #![allow(non_snake_case)]
 
-use const_cstr::*;
-use libc::*;
+use std::ffi::CString;
 use std::mem::transmute;
 
-const RTLD_LAZY: c_int = 0x1;
+const RTLD_LAZY: i32 = 0x1;
 
 extern "C" {
-    fn dlopen(name: *const c_char, flags: c_int) -> *const c_void;
-    fn dlsym(handle: *const c_void, name: *const c_char) -> *const c_void;
+    fn dlopen(name: *const i8, flags: i32) -> *const i8;
+    fn dlsym(handle: *const i8, name: *const i8) -> *const i8;
 }
 
 fn main() {
     unsafe {
-        let handle = dlopen(const_cstr!("libGL.so.1").as_ptr(), RTLD_LAZY);
-        let glXSwapBuffers: unsafe extern "C" fn(dpy: *const c_void, drawable: c_ulong) =
-            { transmute(dlsym(handle, const_cstr!("glXSwapBuffers").as_ptr())) };
-        glXSwapBuffers(0xDEADBEEF as *const c_void, 0);
+        let handle = dlopen(CString::new("libGL.so.1").unwrap().as_ptr(), RTLD_LAZY);
+        let glXSwapBuffers: unsafe extern "C" fn(dpy: *const i8, drawable: u64) = {
+            transmute(dlsym(
+                handle,
+                CString::new("glXSwapBuffers").unwrap().as_ptr(),
+            ))
+        };
+        glXSwapBuffers(0xDEADBEEF as *const i8, 0);
     }
 }
