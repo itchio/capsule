c297d59920733bed58bea51dc07d6c7f9d467b34
Amos Wenger <amoswenger@gmail.com>
2019-04-18 04:09:27 -0700

    Remove all deps for macos-opengl-dyld

--- test/macos-opengl-dyld/src/main.rs
+++ test/macos-opengl-dyld/src/main.rs
@@ -1,14 +1,12 @@
 #![allow(non_snake_case)]
 
-use libc::c_void;
-
 #[link(name = "OpenGL", kind = "framework")]
 extern "C" {
-    fn CGLFlushDrawable(ctx: *const c_void);
+    fn CGLFlushDrawable(ctx: *const i8);
 }
 
 fn main() {
     unsafe {
-        CGLFlushDrawable(0xDEADBEEF as *const c_void);
+        CGLFlushDrawable(0xDEADBEEF as *const i8);
     }
 }
